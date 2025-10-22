e379f211c4229710664e6aa9778345afa9d91316
Willigham Coelho <willigamcoelho@gmail.com>
2025-10-22 16:16:55 -0300

    Commit 16 - Update Tela funcionarios

--- Projeto Integrador/CodigosPython/TelaAtendimento.py
+++ Projeto Integrador/CodigosPython/TelaAtendimento.py
@@ -6,11 +6,16 @@ class TelaAtendimento(QtWidgets.QMainWindow, Ui_FormTelaAtendimento):
     def __init__(self):
         super().__init__()
         self.setupUi(self) # Método gerado para configurar a UI
+        self.pushButtonVoltarAtendimento.clicked.connect(self.voltar)
+        
+    def voltar(self):
+        # Lógica para o botão "Voltar" para acessar a TelaFuncionarios.py"
+        self.close()
         
 
 
 
-app = QtWidgets.QApplication(sys.argv)
-janela = TelaAtendimento()
-janela.show()
-sys.exit(app.exec_())
+# app = QtWidgets.QApplication(sys.argv)
+# janela = TelaAtendimento()
+# janela.show()
+# sys.exit(app.exec_())
--- Projeto Integrador/CodigosPython/TelaFuncionarios.py
+++ Projeto Integrador/CodigosPython/TelaFuncionarios.py
@@ -1,12 +1,24 @@
 import sys
 from PySide6 import QtWidgets
 from ui_FormTelaFuncionarios import  Ui_FormTelaFuncionarios  # Importe a classe gerada
+from TelaAtendimento import TelaAtendimento
 
 class TelaFuncionarios(QtWidgets.QMainWindow, Ui_FormTelaFuncionarios):
     def __init__(self):
         super().__init__()
         self.setupUi(self) # Método gerado para configurar a UI
+        self.pushButtonIniciarAtendimento.clicked.connect(self.atendimento)
+        self.pushButtonSairGenreciadorNegocios.clicked.connect(self.sair)
+    
+        
+    def atendimento(self):
+        self.telaAtendimento = TelaAtendimento()
+        self.telaAtendimento.show()
 
+    def sair(self):
+        self.close()
+        
+        
 
 app = QtWidgets.QApplication(sys.argv)
 janela = TelaFuncionarios()
